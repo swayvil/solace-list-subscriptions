fcbb5b72aaeeaf06f83891c1cc77ada10f476c92
Swayvil <swayvil@gmail.com>
2021-10-11 10:35:51 +0200

    Display queues subscriptions

--- src/main/java/com/solace/review/Subscriptions.java
+++ src/main/java/com/solace/review/Subscriptions.java
@@ -19,6 +19,8 @@
 
 package com.solace.review;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,17 +35,15 @@ public class Subscriptions {
     private final String SEMP_BASE_PATH = "/SEMP/v2/monitor";
     private final int LIMIT_COUNT = 20;
     private MsgVpnApi sempApiInstance;
-    
+
     private void handleError(ApiException ae) {
         Gson gson = new Gson();
         String responseString = ae.getResponseBody();
         SempMetaOnlyResponse respObj = gson.fromJson(responseString, SempMetaOnlyResponse.class);
         SempError errorInfo = respObj.getMeta().getError();
-        System.out.println("Error during operation. Details:" +
-                "\nHTTP Status Code: " + ae.getCode() + 
-                "\nSEMP Error Code: " + errorInfo.getCode() + 
-                "\nSEMP Error Status: " + errorInfo.getStatus() + 
-                "\nSEMP Error Descriptions: " + errorInfo.getDescription());
+        System.out.println("Error during operation. Details:" + "\nHTTP Status Code: " + ae.getCode()
+                + "\nSEMP Error Code: " + errorInfo.getCode() + "\nSEMP Error Status: " + errorInfo.getStatus()
+                + "\nSEMP Error Descriptions: " + errorInfo.getDescription());
     }
 
     private void initialize(String basePath, String user, String password) throws Exception {
@@ -54,11 +54,11 @@ public class Subscriptions {
         sempApiInstance = new MsgVpnApi(thisClient);
     }
 
-    private List<MsgVpnClient> getClients(String messageVpnName) throws ApiException {
+    private List<MsgVpnClient> getClients(String msgVpnName) throws ApiException {
         List<MsgVpnClient> cliList = new ArrayList<MsgVpnClient>();
         String cursor = null;
         do {
-            MsgVpnClientsResponse resp = sempApiInstance.getMsgVpnClients(messageVpnName, LIMIT_COUNT, cursor, null, null);
+            MsgVpnClientsResponse resp = sempApiInstance.getMsgVpnClients(msgVpnName, LIMIT_COUNT, cursor, null, null);
             cliList.addAll(resp.getData());
             if (resp != null && resp.getMeta() != null && resp.getMeta().getPaging() != null) {
                 cursor = resp.getMeta().getPaging().getCursorQuery();
@@ -69,11 +69,13 @@ public class Subscriptions {
         return cliList;
     }
 
-    private List<MsgVpnClientSubscription> getClientSubscriptions(String messageVpnName, String clientName) throws ApiException {
+    private List<MsgVpnClientSubscription> getClientSubscriptions(String msgVpnName, String clientName)
+            throws ApiException {
         List<MsgVpnClientSubscription> clisubList = new ArrayList<MsgVpnClientSubscription>();
         String cursor = null;
         do {
-            MsgVpnClientSubscriptionsResponse resp = sempApiInstance.getMsgVpnClientSubscriptions(messageVpnName, clientName, LIMIT_COUNT, cursor, null, null);
+            MsgVpnClientSubscriptionsResponse resp = sempApiInstance.getMsgVpnClientSubscriptions(msgVpnName,
+                    clientName, LIMIT_COUNT, cursor, null, null);
             clisubList.addAll(resp.getData());
             if (resp != null && resp.getMeta() != null && resp.getMeta().getPaging() != null) {
                 cursor = resp.getMeta().getPaging().getCursorQuery();
@@ -84,9 +86,43 @@ public class Subscriptions {
         return clisubList;
     }
 
-    private void displayClientsSubscriptions(String messageVpnName) throws Exception {
-        System.out.println("=== Review Topic Subscriptions for Message-VPN " + messageVpnName + " ===");
-        List<MsgVpnClient> clientList = getClients(messageVpnName);
+    private List<MsgVpnQueue> getQueues(String msgVpnName) throws ApiException {
+        List<MsgVpnQueue> queueList = new ArrayList<MsgVpnQueue>();
+        String cursor = null;
+        do {
+            MsgVpnQueuesResponse resp = sempApiInstance.getMsgVpnQueues(msgVpnName, LIMIT_COUNT, cursor, null, null);
+            queueList.addAll(resp.getData());
+            if (resp != null && resp.getMeta() != null && resp.getMeta().getPaging() != null) {
+                cursor = resp.getMeta().getPaging().getCursorQuery();
+            } else {
+                cursor = null;
+            }
+        } while (cursor != null);
+        return queueList;
+    }
+
+    private List<MsgVpnQueueSubscription> getQueueSubscriptions(String msgVpnName, String queueName)
+            throws ApiException {
+        List<MsgVpnQueueSubscription> queueSubList = new ArrayList<MsgVpnQueueSubscription>();
+        String cursor = null;
+        do {
+            MsgVpnQueueSubscriptionsResponse resp = sempApiInstance.getMsgVpnQueueSubscriptions(msgVpnName, queueName,
+                    LIMIT_COUNT, cursor, null, null);
+            queueSubList.addAll(resp.getData());
+            if (resp != null && resp.getMeta() != null && resp.getMeta().getPaging() != null) {
+                cursor = resp.getMeta().getPaging().getCursorQuery();
+            } else {
+                cursor = null;
+            }
+        } while (cursor != null);
+        return queueSubList;
+    }
+
+    private String displayClientsSubscriptions(String msgVpnName) throws Exception {
+        String output = "";
+
+        System.out.println("=== Display Topic Subscriptions for Message-VPN " + msgVpnName + " ===");
+        List<MsgVpnClient> clientList = getClients(msgVpnName);
         if (clientList == null) {
             throw new Exception("Can\'t fetch clients list");
         }
@@ -94,19 +130,60 @@ public class Subscriptions {
 
         for (int i = 0; i < clientList.size(); i++) {
             System.out.printf("%s\n", clientList.get(i).getClientName());
-            List<MsgVpnClientSubscription> clisubList = getClientSubscriptions(messageVpnName, clientList.get(i).getClientName());
+            output += clientList.get(i).getClientName() + "\n";
+            List<MsgVpnClientSubscription> clisubList = getClientSubscriptions(msgVpnName,
+                    clientList.get(i).getClientName());
             if (clisubList == null) {
                 throw new Exception("Can\'t fetch topic subscriptions");
             }
             for (int j = 0; j < clisubList.size(); j++) {
+                output += ";" + clisubList.get(j).getSubscriptionTopic() + "\n";
                 System.out.printf("    %s\n", clisubList.get(j).getSubscriptionTopic());
             }
+        }
+        return output;
+    }
+
+    private String displayQueuesSubscriptions(String msgVpnName) throws Exception {
+        String output = "";
+
+        System.out.println("=== Display Topic Subscriptions for Message-VPN " + msgVpnName + " queues ===");
+        List<MsgVpnQueue> queueList = getQueues(msgVpnName);
+        if (queueList == null) {
+            throw new Exception("Can\'t fetch queues list");
+        }
+        System.out.println("Retrieved " + queueList.size() + " Queues");
+
+        for (int i = 0; i < queueList.size(); i++) {
+            output += queueList.get(i).getQueueName() + "\n";
+            System.out.printf("%s\n", queueList.get(i).getQueueName());
+            List<MsgVpnQueueSubscription> queueSubList = getQueueSubscriptions(msgVpnName,
+                    queueList.get(i).getQueueName());
+            if (queueSubList == null) {
+                throw new Exception("Can\'t fetch topic subscriptions");
+            }
+            for (int j = 0; j < queueSubList.size(); j++) {
+                output += ";" + queueSubList.get(j).getSubscriptionTopic() + "\n";
+                System.out.printf("    %s\n", queueSubList.get(j).getSubscriptionTopic());
+            }
+        }
+        return output;
+    }
+
+    public void writeToFile(String filename, String content) {
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write(content);
+            myWriter.close();
+            System.out.println("\n=> Output written to " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String... args) {
         final String usage = "\nUsage: solace-subscriptions-review <broker_mgmt_root_URL> <management_user> <management_password> <vpnname>";
-        
+
         // Check command line arguments
         if (args.length < 4) {
             System.out.println(usage);
@@ -116,13 +193,21 @@ public class Subscriptions {
         String vmrBasePath = args[0];
         String vmrUser = args[1];
         String vmrPassword = args[2];
-        String messageVpnName = args[3];
+        String msgVpnName = args[3];
 
         Subscriptions app = new Subscriptions();
+        String output = "";
 
         try {
             app.initialize(vmrBasePath, vmrUser, vmrPassword);
-            app.displayClientsSubscriptions(messageVpnName);
+            output = "Client name;Topic subscription\n";
+            output += app.displayClientsSubscriptions(msgVpnName);
+            app.writeToFile(msgVpnName + "-clients-subscriptions.csv", output);
+
+            System.out.println("\n");
+            output = "Queue name;Topic subscription\n";
+            output += app.displayQueuesSubscriptions(msgVpnName);
+            app.writeToFile(msgVpnName + "-queues-subscriptions.csv", output);
         } catch (ApiException e) {
             app.handleError(e);
             System.exit(-1);
